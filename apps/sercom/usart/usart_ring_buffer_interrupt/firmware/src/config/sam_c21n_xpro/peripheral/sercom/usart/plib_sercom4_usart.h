6baa9244c05a22d1dbd6b91c2fc010e218dda464
Prachi Phadke <prachi.phadke@microchip.com>
2020-08-13 10:21:00 +0530

    [SERCOM] Regenerated demo for sercom_usart_usart_ring_buffer_interrupt_sam_c21n_xpro
    MH3-39645
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/sercom/usart/usart_ring_buffer_interrupt/firmware/src/config/sam_c21n_xpro/peripheral/sercom/usart/plib_sercom4_usart.h
+++ apps/sercom/usart/usart_ring_buffer_interrupt/firmware/src/config/sam_c21n_xpro/peripheral/sercom/usart/plib_sercom4_usart.h
@@ -89,6 +89,8 @@ void SERCOM4_USART_WriteThresholdSet(uint32_t nBytesThreshold);
 
 void SERCOM4_USART_WriteCallbackRegister( SERCOM_USART_RING_BUFFER_CALLBACK callback, uintptr_t context);
 
+
+
 size_t SERCOM4_USART_Read(uint8_t* pRdBuffer, const size_t size);
 
 size_t SERCOM4_USART_ReadCountGet(void);
