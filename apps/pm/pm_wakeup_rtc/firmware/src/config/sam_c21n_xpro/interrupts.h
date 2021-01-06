d094a9acbbfd86d89005e0bac18ffe93269e8b75
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-01-06 14:15:10 +0530

    [APPS] Re-triggered the python logic to disable SUPC interrupt in pm_wakeup_rtc application
    MH3-47098

--- apps/pm/pm_wakeup_rtc/firmware/src/config/sam_c21n_xpro/interrupts.h
+++ apps/pm/pm_wakeup_rtc/firmware/src/config/sam_c21n_xpro/interrupts.h
@@ -1,20 +1,20 @@
 /*******************************************************************************
-  EVSYS Peripheral Library
+ System Interrupts File
 
   Company:
     Microchip Technology Inc.
 
   File Name:
-    plib_evsys.c
+    interrupt.h
 
   Summary:
-    EVSYS Source File
+    Interrupt vectors mapping
 
   Description:
-    None
+    This file contains declarations of device vectors used by Harmony 3
+ *******************************************************************************/
 
-*******************************************************************************/
-
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
 * Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
 *
@@ -36,16 +36,32 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*******************************************************************************/
+ *******************************************************************************/
+// DOM-IGNORE-END
 
-#include "plib_evsys.h"
-#include "interrupts.h"
+#ifndef INTERRUPTS_H
+#define INTERRUPTS_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include <stdint.h>
 
 
-void EVSYS_Initialize( void )
-{    /*Event Channel User Configuration*/
+// *****************************************************************************
+// *****************************************************************************
+// Section: Handler Routines
+// *****************************************************************************
+// *****************************************************************************
+
+void Reset_Handler (void);
+void NonMaskableInt_Handler (void);
+void HardFault_Handler (void);
+void SysTick_Handler (void);
+void RTC_InterruptHandler (void);
 
 
-}
 
-
+#endif // INTERRUPTS_H
