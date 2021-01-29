f9be9ecfd5afdbe63989f2f005480149da0de7a9
Prachi Phadke <prachi.phadke@microchip.com>
2021-01-29 13:31:05 +0530

    [WDT] Regenerated demo for wdt_wdt_timeout_sam_c21n_xpro
    MH3-48704
    
    Signed-off-by: Prachi Phadke <prachi.phadke@microchip.com>

--- apps/wdt/wdt_timeout/firmware/src/config/sam_c21n_xpro/definitions.h
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_c21n_xpro/definitions.h
@@ -50,6 +50,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
+#include "peripheral/evsys/plib_evsys.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
--- apps/wdt/wdt_timeout/firmware/src/config/sam_c21n_xpro/peripheral/wdt/plib_wdt.c
+++ apps/wdt/wdt_timeout/firmware/src/config/sam_c21n_xpro/peripheral/wdt/plib_wdt.c
@@ -46,6 +46,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
+#include "interrupts.h"
 #include "plib_wdt.h"
 
 
@@ -61,10 +62,13 @@ void WDT_Enable( void )
     if((WDT_REGS->WDT_CTRLA & WDT_CTRLA_ALWAYSON_Msk) != WDT_CTRLA_ALWAYSON_Msk)
     {
         /* Enable Watchdog Timer */
-        WDT_REGS->WDT_CTRLA |= WDT_CTRLA_ENABLE_Msk;
+        WDT_REGS->WDT_CTRLA |= (uint8_t)WDT_CTRLA_ENABLE_Msk;
 
         /* Wait for synchronization */
-        while(WDT_REGS->WDT_SYNCBUSY);
+        while(WDT_REGS->WDT_SYNCBUSY != 0U)
+        {
+
+        }
     }
 }
 
@@ -72,13 +76,19 @@ void WDT_Enable( void )
 void WDT_Disable( void )
 {
     /* Wait for synchronization */
-    while(WDT_REGS->WDT_SYNCBUSY);
+    while(WDT_REGS->WDT_SYNCBUSY != 0U)
+    {
+
+    }
 
     /* Disable Watchdog Timer */
-    WDT_REGS->WDT_CTRLA &= ~(WDT_CTRLA_ENABLE_Msk);
+    WDT_REGS->WDT_CTRLA &= (uint8_t)(~WDT_CTRLA_ENABLE_Msk);
 
     /* Wait for synchronization */
-    while(WDT_REGS->WDT_SYNCBUSY);
+    while(WDT_REGS->WDT_SYNCBUSY != 0U)
+    {
+
+    }
 }
 
 /* If application intends to stay in active mode after clearing WDT, then use WDT_Clear API to clear the WDT. This avoids CPU from waiting or stalling for Synchronization.
@@ -90,7 +100,7 @@ void WDT_Clear( void )
     {
         /* Clear WDT and reset the WDT timer before the
         timeout occurs */
-        WDT_REGS->WDT_CLEAR = WDT_CLEAR_CLEAR_KEY;
+        WDT_REGS->WDT_CLEAR = (uint8_t)WDT_CLEAR_CLEAR_KEY;
     }
 }
 
@@ -101,13 +111,19 @@ void WDT_Clear( void )
 void WDT_ClearWithSync( void )
 {
     /* Wait for synchronization */
-    while(WDT_REGS->WDT_SYNCBUSY);
+    while(WDT_REGS->WDT_SYNCBUSY != 0U)
+    {
+
+    }
 
     /* Clear WDT and reset the WDT timer before the
     timeout occurs */
-    WDT_REGS->WDT_CLEAR = WDT_CLEAR_CLEAR_KEY;
+    WDT_REGS->WDT_CLEAR = (uint8_t)WDT_CLEAR_CLEAR_KEY;
 
     /* Wait for synchronization */
-    while(WDT_REGS->WDT_SYNCBUSY);
+    while(WDT_REGS->WDT_SYNCBUSY != 0U)
+    {
+
+    }
 }
 
