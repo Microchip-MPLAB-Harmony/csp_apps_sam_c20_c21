cd76eb0c93904e90052678a37373a2a9fd196cd0
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-10 18:03:38 +0530

    [EIC] Regenerated demo for eic_eic_led_on_off_sam_c21n_xpro
    MH3-67292
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/eic/eic_led_on_off/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/eic/eic_led_on_off/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -78,14 +77,16 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000;
    PORT_REGS->GROUP[1].PORT_PINCFG[19] = 0x7;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[9] = 0x0;
 
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x20;
    PORT_REGS->GROUP[2].PORT_PINCFG[5] = 0x0;
 
+   PORT_REGS->GROUP[2].PORT_PMUX[2] = 0x0;
+
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
