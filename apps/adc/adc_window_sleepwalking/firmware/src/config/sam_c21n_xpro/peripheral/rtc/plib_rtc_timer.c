41b66397ce60317bb2c605f1bf3c37550f400768
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-10 17:44:26 +0530

    [ADC] Regenerated demo for adc_adc_window_sleepwalking_sam_c21n_xpro
    MH3-67278
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/adc/adc_window_sleepwalking/firmware/src/config/sam_c21n_xpro/peripheral/rtc/plib_rtc_timer.c
+++ apps/adc/adc_window_sleepwalking/firmware/src/config/sam_c21n_xpro/peripheral/rtc/plib_rtc_timer.c
@@ -44,11 +44,12 @@
 #include "interrupts.h"
 #include "plib_rtc.h"
 #include <stdlib.h>
+#include <limits.h>
 
 
 void RTC_Initialize(void)
 {
-    RTC_REGS->MODE0.RTC_CTRLA = (uint16_t)RTC_MODE0_CTRLA_SWRST_Msk;
+    RTC_REGS->MODE0.RTC_CTRLA = RTC_MODE0_CTRLA_SWRST_Msk;
 
     while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_SWRST_Msk) == RTC_MODE0_SYNCBUSY_SWRST_Msk)
     {
@@ -57,8 +58,15 @@ void RTC_Initialize(void)
 
 
     RTC_REGS->MODE0.RTC_CTRLA = (uint16_t)(RTC_MODE0_CTRLA_MODE(0UL) | RTC_MODE0_CTRLA_PRESCALER(0x1UL) | RTC_MODE0_CTRLA_COUNTSYNC_Msk |RTC_MODE0_CTRLA_MATCHCLR_Msk );
-
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COUNTSYNC_Msk) == RTC_MODE0_SYNCBUSY_COUNTSYNC_Msk)
+    {
+       /* Wait for Synchronization */
+    }
    RTC_REGS->MODE0.RTC_COMP = 0x200U;
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COMP0_Msk) == RTC_MODE0_SYNCBUSY_COMP0_Msk)
+    {
+        /* Wait for Synchronization after writing Compare Value */
+    }
 
     RTC_REGS->MODE0.RTC_EVCTRL = 0x100U;
 }
@@ -104,9 +112,29 @@ bool RTC_Timer32CounterHasOverflowed ( void )
 }
 
 
+void RTC_Timer32CountSyncEnable ( void )
+{
+    RTC_REGS->MODE0.RTC_CTRLA |= RTC_MODE0_CTRLA_COUNTSYNC_Msk;
+
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COUNTSYNC_Msk) == RTC_MODE0_SYNCBUSY_COUNTSYNC_Msk)
+    {
+        /* Wait for Synchronization */
+    }
+}
+	
+void RTC_Timer32CountSyncDisable ( void )
+{
+    RTC_REGS->MODE0.RTC_CTRLA &= (uint16_t)(~RTC_MODE0_CTRLA_COUNTSYNC_Msk);
+
+    while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COUNTSYNC_Msk) == RTC_MODE0_SYNCBUSY_COUNTSYNC_Msk)
+    {
+        /* Wait for Synchronization */
+    }
+}
+	
 void RTC_Timer32Start ( void )
 {
-    RTC_REGS->MODE0.RTC_CTRLA |= (uint16_t)RTC_MODE0_CTRLA_ENABLE_Msk;
+    RTC_REGS->MODE0.RTC_CTRLA |= RTC_MODE0_CTRLA_ENABLE_Msk;
 
     while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_ENABLE_Msk) == RTC_MODE0_SYNCBUSY_ENABLE_Msk)
     {
@@ -146,6 +174,15 @@ void RTC_Timer32CompareSet ( uint32_t compareValue )
 }
 uint32_t RTC_Timer32CounterGet ( void )
 {
+    if ((RTC_REGS->MODE0.RTC_CTRLA & RTC_MODE0_CTRLA_COUNTSYNC_Msk) == 0U)
+    {
+        RTC_REGS->MODE0.RTC_CTRLA |= RTC_MODE0_CTRLA_COUNTSYNC_Msk;
+
+        while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COUNTSYNC_Msk) == RTC_MODE0_SYNCBUSY_COUNTSYNC_Msk)
+        {
+           /* Wait for Synchronization */
+        }
+    }
     while((RTC_REGS->MODE0.RTC_SYNCBUSY & RTC_MODE0_SYNCBUSY_COUNT_Msk) == RTC_MODE0_SYNCBUSY_COUNT_Msk)
     {
         /* Wait for Synchronization before reading value from Count Register */
