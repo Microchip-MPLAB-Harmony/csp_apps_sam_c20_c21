ffb653f4fa6b2b4907a985e676de849a3461d03e
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-18 18:48:28 +0530

    [PM] Regenerated demo for pm_pm_wakeup_rtc_sam_c21n_xpro
    MH3-78743

--- apps/pm/pm_wakeup_rtc/firmware/src/config/sam_c21n_xpro/peripheral/rtc/plib_rtc_timer.c
+++ apps/pm/pm_wakeup_rtc/firmware/src/config/sam_c21n_xpro/peripheral/rtc/plib_rtc_timer.c
@@ -44,9 +44,8 @@
 #include "interrupts.h"
 #include "plib_rtc.h"
 #include <stdlib.h>
-#include <limits.h>
 
-static RTC_OBJECT rtcObj;
+volatile static RTC_OBJECT rtcObj;
 
 
 void RTC_Initialize(void)
@@ -82,7 +81,7 @@ void RTC_Timer32CountSyncEnable ( void )
         /* Wait for Synchronization */
     }
 }
-	
+
 void RTC_Timer32CountSyncDisable ( void )
 {
     RTC_REGS->MODE0.RTC_CTRLA &= (uint16_t)(~RTC_MODE0_CTRLA_COUNTSYNC_Msk);
@@ -92,7 +91,7 @@ void RTC_Timer32CountSyncDisable ( void )
         /* Wait for Synchronization */
     }
 }
-	
+
 void RTC_Timer32Start ( void )
 {
     RTC_REGS->MODE0.RTC_CTRLA |= RTC_MODE0_CTRLA_ENABLE_Msk;
@@ -181,7 +180,7 @@ void RTC_Timer32CallbackRegister ( RTC_TIMER32_CALLBACK callback, uintptr_t cont
     rtcObj.context            = context;
 }
 
-void RTC_InterruptHandler( void )
+void __attribute__((used)) RTC_InterruptHandler( void )
 {
     rtcObj.timer32intCause = (RTC_TIMER32_INT_MASK) RTC_REGS->MODE0.RTC_INTFLAG;
     RTC_REGS->MODE0.RTC_INTFLAG = (uint16_t)RTC_MODE0_INTFLAG_Msk;
@@ -190,6 +189,8 @@ void RTC_InterruptHandler( void )
     /* Invoke registered Callback function */
     if(rtcObj.timer32BitCallback != NULL)
     {
-        rtcObj.timer32BitCallback( rtcObj.timer32intCause, rtcObj.context );
+        RTC_TIMER32_INT_MASK timer32intCause = rtcObj.timer32intCause;
+        uintptr_t context = rtcObj.context;
+        rtcObj.timer32BitCallback( timer32intCause, context );
     }
 }
